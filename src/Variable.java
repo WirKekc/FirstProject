dca6587e2132db41e6a6e22a768070f1cb02ad8a
WirKekc <bk9419@yandex.ru>
2021-02-13 19:29:21 +0300

    Initial commit

---
+++ src/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte b = -5;
+        short s = 45;
+        int i = 45620078;
+        long l = 1234567890;
+        float f = 3.4393245f;
+        double d = 45.245356d;
+        char c = 'Z';
+        boolean bool = true;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bool);
+    }
+}
